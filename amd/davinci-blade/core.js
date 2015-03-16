66e883ca8e47ea587c5dc51841486fa7316222b2
David Holmes <david.geo.holmes@gmail.com>
2015-03-16 17:13:44 -0400

    Modify bower.json to include YUI files

--- amd/davinci-blade/core.js
+++ amd/davinci-blade/core.js
@@ -1,7 +1,7 @@
 define(["require", "exports"], function (require, exports) {
     var blade = {
         // TODO: Automatically synchronize with bower.json
-        VERSION: '0.9.2'
+        VERSION: '0.9.3'
     };
     return blade;
 });
--- cjs/davinci-blade/core.js
+++ cjs/davinci-blade/core.js
@@ -1,5 +1,5 @@
 var blade = {
     // TODO: Automatically synchronize with bower.json
-    VERSION: '0.9.2'
+    VERSION: '0.9.3'
 };
 module.exports = blade;
